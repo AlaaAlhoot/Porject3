997023a291ee5ed7c809e7bb6e529f7a9282003b
AlaaAlhoot <87899347+AlaaAlhoot@users.noreply.github.com>
2023-01-23 19:46:55 +0200

    complete
    project

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,4 +1,4 @@
-"""Blog URL Configuration
+"""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -17,7 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views as core_views
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", core_views.listing, name="listing"),
-    path("view_blog/<int:blog_id>/", core_views.view_blog, name="view_blog"),
+path("admin/", admin.site.urls),
+path("", core_views.listing, name="listing"),
+path("view_blog/<int:blog_id>/", core_views.view_blog, name="view_blog"),
+path("see_request/", core_views.see_request),
+path("user_info/", core_views.user_info),
+path("private_place/", core_views.private_place),
+path("accounts/", include("django.contrib.auth.urls")),
+path("staff_place/", core_views.staff_place),
+# add the following url
+path("add_messages/", core_views.add_messages),
 ]
